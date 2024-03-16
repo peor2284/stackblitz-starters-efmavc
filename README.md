c65621d7e03a3a03f6df6a8577c761ca6881b143
peor2284 <163487786+peor2284@users.noreply.github.com>
2024-03-15 19:35:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-efmavc
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/peor2284/stackblitz-starters-efmavc)
---
+++ index.js
@@ -0,0 +1,10 @@
+// run `node index.js` in the terminal
+
+console.log("hola koders")
+const miNombre = "rodolfo"
+let miEdad = 30
+
+const prompt = require("prompt-sync")()
+const nombre = prompt("ingresa tu nombre")
+console.log("hola, "+nombre+" .¡como estas?") //concatenacion: unir una cadena de caracteres con otra
+console.log("hola, ",nombre, "¿como estas?")
